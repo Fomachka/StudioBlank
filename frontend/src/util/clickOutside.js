14eb17c6418562a49c091195f6366b85920fffdf
Man Khi <Fomachka93@yahoo.com>
2024-02-06 13:54:42 +0900

    removed front

--- frontend/src/util/clickOutside.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export function useOutsideClick(ref, onClickOut) {
-  useEffect(() => {
-    const onClick = ({ target }) => !ref?.contains(target) && onClickOut?.();
-    document.addEventListener("click", onClick);
-    return () => document.removeEventListener("click", onClick);
-  });
-}
